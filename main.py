a0d8a03072ab734c0f2a7e14914b0e16d97d6dd6
maki-engineer <ryohei-81@outlook.jp>
2023-05-22 07:01:56 +0900

    add: プロジェクト作成

---
+++ main.py
@@ -0,0 +1,43 @@
+import load_dotenv
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+load_dotenv()
+
+line_bot_api = LineBotApi(os.environ("LINE_API_KEY"))
+handler = WebhookHandler(os.environ("LINE_CHANNEL_SECRET"))
+app = Flask(__name__)
+
+header = {
+    "Content_type": "application/json",
+    "Authorization": "Bearer" + os.environ("LINE_API_KEY")
+}
+
+
+@app.route("/")
+def hello_world():
+    return "Hello world"
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+    print("返信が完了しました。")
